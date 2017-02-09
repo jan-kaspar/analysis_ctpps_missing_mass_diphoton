8c90fa7db7a14f7a4060adb137f9c08ff4809c92
Jan Kašpar <jan.kaspar@cern.ch>
2017-02-09 13:42:14 +0100

    Added 2D hists.

--- distributions.cc
+++ distributions.cc
@@ -1,20 +1,3 @@
-/*
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/one/EDAnalyzer.h"
-
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
-#include "DataFormats/PatCandidates/interface/MET.h"
-
-#include "DiphotonAnalyzer/EventAnalyzer/interface/SelectionUtils.h"
-
-#include "FWCore/ServiceRegistry/interface/Service.h"
-#include "CommonTools/UtilAlgos/interface/TFileService.h"
-*/
-
 #include "DataFormats/FWLite/interface/Handle.h"
 #include "DataFormats/FWLite/interface/ChainEvent.h"
 #include "DataFormats/Math/interface/LorentzVector.h"
@@ -43,55 +26,67 @@ using namespace edm;
 
 struct PlotGroup
 {
-  TH1D *h_diproton_px, *h_diproton_py, *h_diproton_pz, *h_diproton_pT, *h_diproton_m;
-  TH1D *h_diphoton_px, *h_diphoton_py, *h_diphoton_pz, *h_diphoton_pT, *h_diphoton_m;
-  TH1D *h_invis_px, *h_invis_py, *h_invis_pz, *h_invis_pT, *h_invis_m;
+	TH1D *h_diproton_px, *h_diproton_py, *h_diproton_pz, *h_diproton_pT, *h_diproton_m;
+	TH1D *h_diphoton_px, *h_diphoton_py, *h_diphoton_pz, *h_diphoton_pT, *h_diphoton_m;
+	TH1D *h_missing_px, *h_missing_py, *h_missing_pz, *h_missing_pT, *h_missing_m;
 
-  PlotGroup()
-  {
-  }
+	TH2D *h2_diph_pT_vs_diph_m, *h2_miss_m_vs_diph_m, *h2_miss_m_vs_dipr_m, *h2_miss_m_vs_diph_pT;
 
-  void Init()
-  {
-    h_diproton_px = new TH1D("", "", 100, -500., +500);
-    h_diproton_py = new TH1D("", "", 100, -500., +500.);
-    h_diproton_pz = new TH1D("", "", 100, -1000., +1000.);
-    h_diproton_pT = new TH1D("", "", 100, 0., 500.);
-    h_diproton_m = new TH1D("", "", 100, 10e3, 13e3);
-    
-    h_diphoton_px = new TH1D("", "", 100, -500., +500.);
-    h_diphoton_py = new TH1D("", "", 100, -500., +500.);
-    h_diphoton_pz = new TH1D("", "", 100, -1000., +1000.);
-    h_diphoton_pT = new TH1D("", "", 100, 0., +500.);
-    h_diphoton_m = new TH1D("", "", 100, 0., +1000.);
-    
-    h_invis_px = new TH1D("", "", 100, -500., +500.);
-    h_invis_py = new TH1D("", "", 100, -500., +500.);
-    h_invis_pz = new TH1D("", "", 100, -1500., +1500.);
-    h_invis_pT = new TH1D("", "", 100, 0., +500.);
-    h_invis_m = new TH1D("", "", 100, -2000., +2000.);
-  }
+	PlotGroup()
+	{
+	}
 
-  void Write() const
-  {
-    h_diproton_px->Write("h_diproton_px");
-    h_diproton_py->Write("h_diproton_py");
-    h_diproton_pz->Write("h_diproton_pz");
-    h_diproton_pT->Write("h_diproton_pT");
-    h_diproton_m->Write("h_diproton_m");
-    
-    h_diphoton_px->Write("h_diphoton_px");
-    h_diphoton_py->Write("h_diphoton_py");
-    h_diphoton_pz->Write("h_diphoton_pz");
-    h_diphoton_pT->Write("h_diphoton_pT");
-    h_diphoton_m->Write("h_diphoton_m");
-    
-    h_invis_px->Write("h_invis_px");
-    h_invis_py->Write("h_invis_py");
-    h_invis_pz->Write("h_invis_pz");
-    h_invis_pT->Write("h_invis_pT");
-    h_invis_m->Write("h_invis_m");
-  }
+	void Init()
+	{
+		h_diproton_px = new TH1D("", "", 100, -500., +500);
+		h_diproton_py = new TH1D("", "", 100, -500., +500.);
+		h_diproton_pz = new TH1D("", "", 100, -1000., +1000.);
+		h_diproton_pT = new TH1D("", "", 100, 0., 500.);
+		h_diproton_m = new TH1D("", "", 100, 10e3, 13e3);
+		
+		h_diphoton_px = new TH1D("", "", 100, -500., +500.);
+		h_diphoton_py = new TH1D("", "", 100, -500., +500.);
+		h_diphoton_pz = new TH1D("", "", 100, -1000., +1000.);
+		h_diphoton_pT = new TH1D("", "", 100, 0., +1000.);
+		h_diphoton_m = new TH1D("", "", 100, 0., +1500.);
+		
+		h_missing_px = new TH1D("", "", 100, -500., +500.);
+		h_missing_py = new TH1D("", "", 100, -500., +500.);
+		h_missing_pz = new TH1D("", "", 100, -1500., +1500.);
+		h_missing_pT = new TH1D("", "", 100, 0., +500.);
+		h_missing_m = new TH1D("", "", 100, -2500., +2500.);
+
+		h2_diph_pT_vs_diph_m = new TH2D("", ";diph m;diph pT", 100, 0., 1500., 100, 0., 1000.);
+		h2_miss_m_vs_diph_m = new TH2D("", ";diph m;miss m", 100, 0., 1500., 100, -2500., +2500.);
+		h2_miss_m_vs_dipr_m = new TH2D("", ";dipr m;miss m", 100, 10E3, 13E3, 100, -2500., +2500.);
+		h2_miss_m_vs_diph_pT = new TH2D("", ";diph pT;miss m", 100, 0., 1000., 100, -2500., +2500.);
+	}
+
+	void Write() const
+	{
+		h_diproton_px->Write("h_diproton_px");
+		h_diproton_py->Write("h_diproton_py");
+		h_diproton_pz->Write("h_diproton_pz");
+		h_diproton_pT->Write("h_diproton_pT");
+		h_diproton_m->Write("h_diproton_m");
+		
+		h_diphoton_px->Write("h_diphoton_px");
+		h_diphoton_py->Write("h_diphoton_py");
+		h_diphoton_pz->Write("h_diphoton_pz");
+		h_diphoton_pT->Write("h_diphoton_pT");
+		h_diphoton_m->Write("h_diphoton_m");
+		
+		h_missing_px->Write("h_missing_px");
+		h_missing_py->Write("h_missing_py");
+		h_missing_pz->Write("h_missing_pz");
+		h_missing_pT->Write("h_missing_pT");
+		h_missing_m->Write("h_missing_m");
+
+		h2_diph_pT_vs_diph_m->Write("h2_diph_pT_vs_diph_m");
+		h2_miss_m_vs_diph_m->Write("h2_miss_m_vs_diph_m");
+		h2_miss_m_vs_dipr_m->Write("h2_miss_m_vs_dipr_m");
+		h2_miss_m_vs_diph_pT->Write("h2_miss_m_vs_diph_pT");
+	}
 };
 
 //----------------------------------------------------------------------------------------------------
@@ -101,10 +96,10 @@ void PrintUsage()
 {
 	printf("USAGE: distributions [options]\n");
 	printf("OPTIONS:\n");
-	printf("    -input <file>         file with list of input files\n");
-	printf("    -min-idx <number>     first file index\n");
-	printf("    -max-idx <number>     last file index\n");
-	printf("    -output <filename>    output file name\n");
+	printf("		-input <file>				 file with list of input files\n");
+	printf("		-min-idx <number>		 first file index\n");
+	printf("		-max-idx <number>		 last file index\n");
+	printf("		-output <filename>		output file name\n");
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -193,7 +188,7 @@ int main(int argc, char **argv)
 
 	printf("> input files\n");
 	for (const auto &f : input_files)
-		printf("    %s\n", f.c_str());
+		printf("		%s\n", f.c_str());
 
 	fwlite::ChainEvent event(input_files);
 
@@ -215,7 +210,7 @@ int main(int argc, char **argv)
 	{
 		// get trigger data
 		fwlite::Handle<TriggerResults> triggerResults;
-  		triggerResults.getByLabel(event, "TriggerResults", "", "HLT");
+			triggerResults.getByLabel(event, "TriggerResults", "", "HLT");
 
 		const TriggerNames &triggerNames = event.triggerNames(*triggerResults);
 
@@ -242,51 +237,56 @@ int main(int argc, char **argv)
 		diphpr.getByLabel(event, "flashggDiProtonsDiPhotons");
 
 		for (unsigned int i = 0; i < diphpr->size(); i++)
-  		{
-    		//Ptr<flashgg::DiProtonDiPhotonCandidate> pc = diphpr->ptrAt(i);
-    		const flashgg::DiProtonDiPhotonCandidate &pc = diphpr->at(i);
+		{
+			//Ptr<flashgg::DiProtonDiPhotonCandidate> pc = diphpr->ptrAt(i);
+			const flashgg::DiProtonDiPhotonCandidate &pc = diphpr->at(i);
 	
-		    // kinematics
-		    double p0 = 6500.;  // GeV
+			// kinematics
+			double p0 = 6500.;	// GeV
 		
-		    LorentzVector p4_init(0., 0., 0., 2.* p0);
+			LorentzVector p4_init(0., 0., 0., 2.* p0);
 		
-		    LorentzVector p4_diphoton = pc.diphoton()->p4();
+			LorentzVector p4_diphoton = pc.diphoton()->p4();
 		
-		    // xi's are positive
-		    // without guarantee: proton1 is left arm, proton2 is right arm
-		    double xi_L = pc.diproton()->proton1()->xi();
-		    double xi_R = pc.diproton()->proton2()->xi();
+			// xi's are positive
+			// without guarantee: proton1 is left arm, proton2 is right arm
+			double xi_L = pc.diproton()->proton1()->xi();
+			double xi_R = pc.diproton()->proton2()->xi();
 		
-		    // TODO: verify L/R conventions TOTEM vs. CMS
-		    LorentzVector p4_diproton(0., 0., p0*(xi_L - xi_R), p0*(2. - xi_L - xi_R));
+			// TODO: verify L/R conventions TOTEM vs. CMS
+			LorentzVector p4_diproton(0., 0., p0*(xi_L - xi_R), p0*(2. - xi_L - xi_R));
 		
-		    LorentzVector p4_invis = p4_init - p4_diproton - p4_diphoton;
+			LorentzVector p4_missing = p4_init - p4_diproton - p4_diphoton;
 		
-		    // fill after-cuts histograms
-		    p_without_cuts.h_diproton_px->Fill(p4_diproton.px());
-		    p_without_cuts.h_diproton_py->Fill(p4_diproton.py());
-		    p_without_cuts.h_diproton_pz->Fill(p4_diproton.pz());
-		    p_without_cuts.h_diproton_pT->Fill(p4_diproton.pt());
-		    p_without_cuts.h_diproton_m->Fill(p4_diproton.mass());
+			// fill after-cuts histograms
+			p_without_cuts.h_diproton_px->Fill(p4_diproton.px());
+			p_without_cuts.h_diproton_py->Fill(p4_diproton.py());
+			p_without_cuts.h_diproton_pz->Fill(p4_diproton.pz());
+			p_without_cuts.h_diproton_pT->Fill(p4_diproton.pt());
+			p_without_cuts.h_diproton_m->Fill(p4_diproton.mass());
 		
-		    p_without_cuts.h_diphoton_px->Fill(p4_diphoton.px());
-		    p_without_cuts.h_diphoton_py->Fill(p4_diphoton.py());
-		    p_without_cuts.h_diphoton_pz->Fill(p4_diphoton.pz());
-		    p_without_cuts.h_diphoton_pT->Fill(p4_diphoton.pt());
-		    p_without_cuts.h_diphoton_m->Fill(p4_diphoton.mass());
+			p_without_cuts.h_diphoton_px->Fill(p4_diphoton.px());
+			p_without_cuts.h_diphoton_py->Fill(p4_diphoton.py());
+			p_without_cuts.h_diphoton_pz->Fill(p4_diphoton.pz());
+			p_without_cuts.h_diphoton_pT->Fill(p4_diphoton.pt());
+			p_without_cuts.h_diphoton_m->Fill(p4_diphoton.mass());
 		
-		    p_without_cuts.h_invis_px->Fill(p4_invis.px());
-		    p_without_cuts.h_invis_py->Fill(p4_invis.py());
-		    p_without_cuts.h_invis_pz->Fill(p4_invis.pz());
-		    p_without_cuts.h_invis_pT->Fill(p4_invis.pt());
-		    p_without_cuts.h_invis_m->Fill(p4_invis.mass());
+			p_without_cuts.h_missing_px->Fill(p4_missing.px());
+			p_without_cuts.h_missing_py->Fill(p4_missing.py());
+			p_without_cuts.h_missing_pz->Fill(p4_missing.pz());
+			p_without_cuts.h_missing_pT->Fill(p4_missing.pt());
+			p_without_cuts.h_missing_m->Fill(p4_missing.mass());
+
+			p_without_cuts.h2_diph_pT_vs_diph_m->Fill(p4_diphoton.mass(), p4_diphoton.pt());
+			p_without_cuts.h2_miss_m_vs_diph_m->Fill(p4_diphoton.mass(), p4_missing.mass());
+			p_without_cuts.h2_miss_m_vs_dipr_m->Fill(p4_diproton.mass(), p4_missing.mass());
+			p_without_cuts.h2_miss_m_vs_diph_pT->Fill(p4_diphoton.pt(), p4_missing.mass());
 		}
 	}
 
 	// save plots
-  	gDirectory = f_out->mkdir("without cuts");
-  	p_without_cuts.Write();
+	gDirectory = f_out->mkdir("without cuts");
+	p_without_cuts.Write();
 
 	// clean up
 	delete f_out;
